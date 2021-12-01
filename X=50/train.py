9a2cc44419c9ac0684725a054a46aca0ec706a94
biomg <40063814+biomg@users.noreply.github.com>
2021-12-01 20:19:13 +0800

    Add files via upload

---
+++ X=50/train.py
@@ -0,0 +1,18 @@
+import subprocess
+class RunCmd(object):
+  def cmd_run(self, cmd):
+    self.cmd = cmd
+    subprocess.call(self.cmd, shell=True)
+
+for i in range(10):
+    a = RunCmd()
+    a.cmd_run('CUDA_VISIBLE_DEVICES=0 python mcnn.py \
+    --posi=../GraphProt_CLIP_sequences/ALKBH5_Baltz2012.train.positives.fa \
+    --nega=../GraphProt_CLIP_sequences/ALKBH5_Baltz2012.train.negatives.fa \
+    --model_type=CNN --train=True --n_epochs=50')
+    for j in range(10):
+        a = RunCmd()
+        a.cmd_run('CUDA_VISIBLE_DEVICES=0 python mcnn.py \
+        --testfile=../GraphProt_CLIP_sequences/ALKBH5_Baltz2012.ls.positives.fa \
+        --nega=../GraphProt_CLIP_sequences/ALKBH5_Baltz2012.ls.negatives.fa \
+        --model_type=CNN --predict=True')
